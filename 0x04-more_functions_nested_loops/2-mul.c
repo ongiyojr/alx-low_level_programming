88d471c2a354bb6abdc3e127215c6e8ad16851f9
ongiyojr <ongiyowriter@gmail.com>
2023-06-22 15:36:15 -0700

    more functions

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _mul - Entry point
+ * @x: The first number
+ * @y: The second number
+ * Return: The result of x and y
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
